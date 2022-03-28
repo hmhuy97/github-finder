b616fcfd4939fa7aad9922e7a1834bdf5c38c65e
Huỳnh Minh Huy <39719651+hmhuy97@users.noreply.github.com>
2022-03-28 14:14:34 +0700

    3. Get Single User

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import About from "./pages/About";
 import NotFound from "./pages/NotFound";
 import Alert from "./components/layout/Alert";
 import UserResults from "./components/users/UserResults";
+import User from "./pages/User";
 import { GithubProvider } from "./context/github/GithubContext";
 import { AlertProvider } from "./context/alert/AlertContext";
 function App() {
@@ -22,6 +23,7 @@ function App() {
                 <Route path="/" element={<Home />} />
                 <Route path="/about" element={<About />} />
                 <Route path="/notfound" element={<NotFound />} />
+                <Route path="/user/:login" element={<User />} />
                 <Route path="/*" element={<NotFound />} />
               </Routes>
             </main>
--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -8,6 +8,7 @@ const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
 export const GithubProvider = ({ children }) => {
   const initialState = {
     users: [],
+    user: {},
     loading: false,
   };
 
@@ -28,6 +29,22 @@ export const GithubProvider = ({ children }) => {
     dispatch({ type: "GET_USERS", payload: items });
   };
 
+  //   Get single user (testing purposes)
+  const getUser = async (login) => {
+    setLoading();
+
+    const response = await fetch(`${GITHUB_URL}/users/${login}`, {
+      headers: { Authorization: `token ${GITHUB_TOKEN}` },
+    });
+
+    if (response.status === 404) {
+      window.location = "/notfound";
+    } else {
+      const data = await response.json();
+      dispatch({ type: "GET_USER", payload: data });
+    }
+  };
+
   const clearUsers = () => {
     dispatch({ type: "CLEAR_USERS" });
   };
@@ -42,8 +59,10 @@ export const GithubProvider = ({ children }) => {
       value={{
         users: state.users,
         loading: state.loading,
+        user: state.user,
         searchUsers,
         clearUsers,
+        getUser,
       }}
     >
       {children}
